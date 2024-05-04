d6b9bed5009ccf9df988c146e4860b0b375c0a4b
PaxuitoGIT <bxu@myuax.com>
2024-05-04 14:43:34 +0200

    Constructor Experimento

--- src/main/java/CultivoBacterias/Lógica/Experimento.java
+++ src/main/java/CultivoBacterias/Lógica/Experimento.java
@@ -1,7 +1,12 @@
 package CultivoBacterias.Lógica;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Experimento {
     private List<PoblacionBacterias> poblaciones;
+
+    public Experimento() {
+        this.poblaciones = new ArrayList<>();
+    }
 }
