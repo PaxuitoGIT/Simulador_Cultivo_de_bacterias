641bdbb112535a17c9c32b249c656e6315c8c577
PaxuitoGIT <bxu@myuax.com>
2024-05-04 14:48:08 +0200

    Atributos DosisAlimento

---
+++ src/main/java/CultivoBacterias/Lógica/DosisAlimento.java
@@ -0,0 +1,8 @@
+package CultivoBacterias.Lógica;
+
+public class DosisAlimento {
+    private int comidaInicial;
+    private int diaIncremento;
+    private int comidaIncremento;
+    private int comidaFinal;
+}
