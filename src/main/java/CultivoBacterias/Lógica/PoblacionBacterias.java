f6587deaca09b74c451a4ad90c4e9054028f3fe0
PaxuitoGIT <bxu@myuax.com>
2024-05-04 14:39:43 +0200

    Constructor PoblacionBacterias

--- src/main/java/CultivoBacterias/Lógica/PoblacionBacterias.java
+++ src/main/java/CultivoBacterias/Lógica/PoblacionBacterias.java
@@ -10,4 +10,14 @@ public class PoblacionBacterias {
     private int temperatura;
     private String luminosidad;
     private DosisAlimento dosisAlimento;
+
+    public PoblacionBacterias(String nombre, Date fechaInicio, Date fechaFin, int numBacterias, int temperatura, String luminosidad, DosisAlimento dosisAlimento) {
+        this.nombre = nombre;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.numBacterias = numBacterias;
+        this.temperatura = temperatura;
+        this.luminosidad = luminosidad;
+        this.dosisAlimento = dosisAlimento;
+    }
 }
