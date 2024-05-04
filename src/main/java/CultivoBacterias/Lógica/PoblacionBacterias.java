a9205f74161bda0d79559881634540463fd3ca28
PaxuitoGIT <bxu@myuax.com>
2024-05-04 14:40:55 +0200

    Getters y setters PoblacionBacterias

--- src/main/java/CultivoBacterias/Lógica/PoblacionBacterias.java
+++ src/main/java/CultivoBacterias/Lógica/PoblacionBacterias.java
@@ -20,4 +20,60 @@ public class PoblacionBacterias {
         this.luminosidad = luminosidad;
         this.dosisAlimento = dosisAlimento;
     }
+
+    public DosisAlimento getDosisAlimento() {
+        return dosisAlimento;
+    }
+
+    public void setDosisAlimento(DosisAlimento dosisAlimento) {
+        this.dosisAlimento = dosisAlimento;
+    }
+
+    public String getLuminosidad() {
+        return luminosidad;
+    }
+
+    public void setLuminosidad(String luminosidad) {
+        this.luminosidad = luminosidad;
+    }
+
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public int getNumBacterias() {
+        return numBacterias;
+    }
+
+    public void setNumBacterias(int numBacterias) {
+        this.numBacterias = numBacterias;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
