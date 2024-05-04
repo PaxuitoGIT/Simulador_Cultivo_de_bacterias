a8f823ae996b1e53e7eca138067b3fbabb922370
PaxuitoGIT <bxu@myuax.com>
2024-05-04 14:39:24 +0200

    Atributos PoblacionBacterias

---
+++ src/main/java/CultivoBacterias/Lógica/PoblacionBacterias.java
@@ -0,0 +1,13 @@
+package CultivoBacterias.Lógica;
+
+import java.util.Date;
+
+public class PoblacionBacterias {
+    private String nombre;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private int numBacterias;
+    private int temperatura;
+    private String luminosidad;
+    private DosisAlimento dosisAlimento;
+}
