9a6af149d7011ab89d64f9efa8dab1d1fa2b7ec8
PaxuitoGIT <bxu@myuax.com>
2024-05-04 14:44:57 +0200

    Métodos Experimento

--- src/main/java/CultivoBacterias/Lógica/Experimento.java
+++ src/main/java/CultivoBacterias/Lógica/Experimento.java
@@ -9,4 +9,16 @@ public class Experimento {
     public Experimento() {
         this.poblaciones = new ArrayList<>();
     }
+
+    public void agregarPoblacion(PoblacionBacterias poblacion) {
+        poblaciones.add(poblacion);
+    }
+
+    public void eliminarPoblacion(PoblacionBacterias poblacion) {
+        poblaciones.remove(poblacion);
+    }
+
+    public List<PoblacionBacterias> getPoblaciones() {
+        return poblaciones;
+    }
 }
